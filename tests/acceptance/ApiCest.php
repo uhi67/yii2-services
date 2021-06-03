6acd4da9c1be10512dea8767e6bdbc802ab0f373
Uherkovich Péter <uherkovich.peter@gmail.com>
2021-06-04 00:15:23 +0200

    Using corrected SOAP module; acceptance test

--- tests/acceptance/ApiCest.php
+++ tests/acceptance/ApiCest.php
@@ -3,6 +3,8 @@
 namespace acceptance;
 
 use AcceptanceTester;
+use SoapClient;
+use SoapFault;
 
 class ApiCest
 {
@@ -22,37 +24,27 @@ class ApiCest
     }
 
 	/**
-	 * Ez a teszt a fejlesztői verziót "kívülről" teszteli (webszervenek mennie kell)
+	 * Acceptance test runs soap call thru a web server.
+     * Please start web server on port 8080, e.g. `php tests/app/yii serve`
 	 *
-	 * @throws \SoapFault
+	 * @throws SoapFault
 	 */
 	function mirrorWsTest(AcceptanceTester $I) {
-		// int org_id, bool sub_orgs, bool recursive, int client_id, string shared_secret
-		$params = ['a'=>13];
+		$params = 13;
 		$wsdl = 'http://localhost:8080/api/soap';
 		$method = 'mirror';
-		$p = $method.'Result';
 
-		$client = new \SoapClient($wsdl, [
+		$client = new SoapClient($wsdl, [
 			'cache_wsdl'=>WSDL_CACHE_NONE,
 			'cache_wsdl_ttl'=>0,
 		]);
 
 		$soapResult = $client->__soapCall($method, ['parameters'=>$params], ['exceptions' => 1]);
+        codecept_debug('Result='.print_r($soapResult, true));
 
 		$I->assertFalse(is_soap_fault($soapResult));
-
-		$I->assertTrue(isset($soapResult->$p));
-		$xml = simplexml_load_string($soapResult->$p);
-		$expected = <<<EOT
-<?xml version="1.0" encoding="UTF-8"?>
-<ROOT xmlns="">
-  <SERVER_INFO CLIENT_IP="127.0.0.1" CLIENT_ID="department"/>
-  <ORG ORG_ID="1" ORG_NAME="Egyetemi polgárok szervezetei" IS_DELETED="False" TYPE="0" NEXON_KOD="" PARENT_ID="0" FULL_PATH="Pécsi Tudományegyetem / Egyetemi polgárok szervezetei" K2_ID="0"/>
-</ROOT>
-
-EOT;
-		$I->assertEquals(str_replace("\r\n", "\n", $expected), $xml->asXML());
+        $expected = 13;
+		$I->assertEquals($expected, $soapResult);
 
 	}
 }
