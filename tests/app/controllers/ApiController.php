d0f2449291e75f4f2065d51638ce9f75daa9139b
Uherkovich Péter <uherkovich.peter@gmail.com>
2021-06-04 16:09:29 +0200

    new test cases are added;

--- tests/app/controllers/ApiController.php
+++ tests/app/controllers/ApiController.php
@@ -7,6 +7,7 @@
 
 namespace uhi67\services\tests\app\controllers;
 
+use stdClass;
 use uhi67\services\WebServiceAction;
 use uhi67\services\WsdlGenerator;
 use yii\web\Controller;
@@ -71,7 +72,62 @@ class ApiController extends Controller
 		return $aaa;
 	}
 
-	public function actionHello() {
+    /**
+     * @param array $params -- associative array
+     * @return stdClass -- input array in object form
+     * @soap
+     */
+    public function getObject($params)
+    {
+        $object = new stdClass();
+        $object_result = new stdClass();
+        foreach($params as $name=>$value) $object_result->$name = $value;
+        $result_name = __FUNCTION__.'Result';
+        $object->$result_name = $object_result;
+        return $object;
+    }
+
+    /**
+     * @param int $a
+     * @param bool $b
+     * @param string $c
+     * @return \uhi67\services\tests\app\controllers\MyObject -- input values in object form
+     * @soap
+     * @noinspection PhpUnnecessaryFullyQualifiedNameInspection  -- dont use aliases in SOAP phpdoc blocks!
+     */
+    public function getObject2($a, $b, $c)
+    {
+        $object = new MyObject();
+        $object->a = (int)$a;
+        $object->b = (bool)$b;
+        $object->c = $c;
+        return $object;
+    }
+
+    public function actionHello()
+    {
 		return 'Hello World';
 	}
+}
+
+/**
+ * Class MyObject
+ * @soap
+ */
+class MyObject {
+    /**
+     * @var int $a
+     * @soap
+     */
+    public $a;
+    /**
+     * @var bool $b
+     * @soap
+     */
+    public $b;
+    /**
+     * @var string $c
+     * @soap
+     */
+    public $c;
 }
--- tests/soap/SoapCest.php
+++ tests/soap/SoapCest.php
@@ -3,6 +3,8 @@
 namespace soap;
 
 use Codeception\Exception\ModuleException;
+use Codeception\Module\XmlAsserts;
+use Codeception\Util\Soap;
 use SoapTester;
 
 class SoapCest {
@@ -30,13 +32,14 @@ class SoapCest {
 	 */
 	public function mirrorTest(SoapTester $I)
     {
+        $soapEnvScheme = \Codeception\Module\SOAP::SCHEME_SOAP_ENVELOPE;
 	    $namespace = 'urn:uhi67/services/tests/app/controllers/ApiControllerwsdl';
 	    $method = 'mirror';
 	    $I->sendSoapRequest($method, '<aaa>x</aaa>');
 	    $expectedRequest = <<<EOT
-<soapenv:Envelope xmlns:ns="$namespace" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
-	<soapenv:Header xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"/>
-	<soapenv:Body xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+<soapenv:Envelope xmlns:ns="$namespace" xmlns:soapenv="$soapEnvScheme">
+	<soapenv:Header/>
+	<soapenv:Body>
 		<ns:$method>
 			<aaa>x</aaa>
 		</ns:$method>
@@ -51,7 +54,7 @@ EOT;
         $expectedResponse = <<<EOT
 <SOAP-ENV:Envelope
 		xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" 
-		xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
+		xmlns:SOAP-ENV="$soapEnvScheme" 
 		xmlns:ns1="$namespace" 
 		xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
@@ -72,5 +75,73 @@ EOT;
 	    $I->cantSeeSoapResponseContainsXPath('//SOAP-ENV:Envelope/SOAP-ENV:Body/SOAP-ENV:Fault');
 	    $I->seeSoapResponseContainsXPath('//ns1:mirrorResponse');
         $I->seeSoapResponseContainsXPath("//return[text()='x']");
+    }
+
+
+    /**
+     * @throws ModuleException
+     */
+    public function objectTest(SoapTester $I)
+    {
+        $method = 'getObject';
+        $I->sendSoapRequest($method, '<params><a>x</a><b>13</b><c>29</c></params>');
+        $response = $I->grabSoapResponse(); // Only the corrected SOAP module delivers the result.
+        codecept_debug('Response='.$response->saveXML());
+        $I->seeSoapResponseContainsXPath('//ns1:getObjectResponse');
+
+        $expectedResult = /** @lang */ <<<EOT
+<return xsi:type="SOAP-ENC:Struct" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <getObjectResult xsi:type="SOAP-ENC:Struct">
+        <a xsi:type="xsd:string">x</a><b xsi:type="xsd:string">13</b><c xsi:type="xsd:string">29</c>
+    </getObjectResult>
+</return>
+EOT;
+
+        $resultList = XmlAsserts::xmlEval('//ns1:getObjectResponse/return', $response, ['ns1'=>"http://localhost:8080/api/index"]);
+        $I->assertCount(1, $resultList);
+        $I->assertXmlStringEqualsXmlString($expectedResult, XmlAsserts::toXml($resultList[0]));
+    }
+
+    /**
+     * @throws ModuleException
+     */
+    public function object2Test(SoapTester $I)
+    {
+        $soapEnvScheme = \Codeception\Module\SOAP::SCHEME_SOAP_ENVELOPE;
+        $namespace = 'urn:uhi67/services/tests/app/controllers/ApiControllerwsdl';
+        $method = 'getObject2';
+        /** @noinspection PhpUndefinedFieldInspection */
+        $I->sendSoapRequest($method, Soap::request()->a->val(13)->parent()->b->val(true)->parent()->c->val('foo'));
+
+        $expectedRequest = <<<EOT
+<soapenv:Envelope xmlns:ns="$namespace" xmlns:soapenv="$soapEnvScheme">
+	<soapenv:Header/>
+	<soapenv:Body>
+		<ns:$method>
+            <a>13</a>
+            <b>1</b>
+            <c>foo</c>
+		</ns:$method>
+	</soapenv:Body>
+</soapenv:Envelope>
+EOT;
+        $request = $I->grabSoapRequest();
+        $I->assertXmlStringEqualsXmlString($expectedRequest, $request->saveXML());
+
+        $response = $I->grabSoapResponse(); // Only the corrected SOAP module delivers the result.
+        codecept_debug('Response='.$response->saveXML());
+        $I->seeSoapResponseContainsXPath('//ns1:getObject2Response');
+
+        $expectedResult = /** @lang */<<<EOT
+<return xsi:type="SOAP-ENC:Struct" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <a xsi:type="xsd:int">13</a>
+    <b xsi:type="xsd:boolean">true</b>
+    <c xsi:type="xsd:string">foo</c>
+</return>
+EOT;
+
+        $resultList = XmlAsserts::xmlEval('//ns1:getObject2Response/return', $response, ['ns1'=>"http://localhost:8080/api/index"]);
+        $I->assertCount(1, $resultList);
+        $I->assertXmlStringEqualsXmlString($expectedResult, XmlAsserts::toXml($resultList[0]));
     }
 }
